74a46ab6c43973f48bc4ed4c309386d0a141bb20
kzhamaji <kzhamaji@gmail.com>
2017-10-15 19:04:39 +0900

    Add "SearchPostLimit" SQL configulation.
    Conflicts:
    	app/diagnostics.go
    
    Conflicts:
    	model/config.go
    
    Conflicts:
    	app/diagnostics.go
    	model/config.go

--- model/config.go
+++ model/config.go
@@ -654,6 +654,7 @@ type SqlSettings struct {
 	Trace                       bool
 	AtRestEncryptKey            string
 	QueryTimeout                *int
+	SearchPostLimit             *int
 }
 
 func (s *SqlSettings) SetDefaults() {
@@ -683,6 +684,10 @@ func (s *SqlSettings) SetDefaults() {
 
 	if s.QueryTimeout == nil {
 		s.QueryTimeout = NewInt(30)
+	}
+
+	if s.SearchPostLimit == nil {
+		s.SearchPostLimit = NewInt(100)
 	}
 }
 
@@ -1003,7 +1008,6 @@ func (s *SupportSettings) SetDefaults() {
 	if !IsSafeLink(s.TermsOfServiceLink) {
 		*s.TermsOfServiceLink = SUPPORT_SETTINGS_DEFAULT_TERMS_OF_SERVICE_LINK
 	}
-
 	if s.TermsOfServiceLink == nil {
 		s.TermsOfServiceLink = NewString(SUPPORT_SETTINGS_DEFAULT_TERMS_OF_SERVICE_LINK)
 	}
