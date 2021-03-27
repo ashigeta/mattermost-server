1665bb94df5b96c7329ed2ec2e4a279aab0ba4d6
ashigeta <ashigeta@DESKTOP-S9CS7A1.localdomain>
2021-03-27 14:01:54 -0700

    change bleve analyzer to cjk to support japanese

--- services/searchengine/bleveengine/bleve.go
+++ services/searchengine/bleveengine/bleve.go
@@ -13,7 +13,8 @@ import (
 
 	"github.com/blevesearch/bleve"
 	"github.com/blevesearch/bleve/analysis/analyzer/keyword"
-	"github.com/blevesearch/bleve/analysis/analyzer/standard"
+	//"github.com/blevesearch/bleve/analysis/analyzer/standard"
+	"github.com/blevesearch/bleve/analysis/lang/cjk"
 	"github.com/blevesearch/bleve/mapping"
 
 	"github.com/mattermost/mattermost-server/v5/jobs"
@@ -50,7 +51,8 @@ func init() {
 	keywordMapping.Analyzer = keyword.Name
 
 	standardMapping = bleve.NewTextFieldMapping()
-	standardMapping.Analyzer = standard.Name
+	//standardMapping.Analyzer = standard.Name
+	standardMapping.Analyzer = cjk.AnalyzerName
 
 	dateMapping = bleve.NewNumericFieldMapping()
 }
