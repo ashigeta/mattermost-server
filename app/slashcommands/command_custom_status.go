e73eb5d20603401355572c94b6c18a02362cfe37
ashigeta <ashigeta@DESKTOP-S9CS7A1.localdomain>
2021-04-25 19:06:29 -0700

    customize status slash command

--- app/slashcommands/command_custom_status.go
+++ app/slashcommands/command_custom_status.go
@@ -19,6 +19,11 @@ type CustomStatusProvider struct {
 const (
 	CmdCustomStatus      = app.CmdCustomStatusTrigger
 	CmdCustomStatusClear = "clear"
+        CmdCustomStatusPrev  = "prev"
+        CmdCustomStatusAway  = "away"
+        CmdCustomStatusHome  = "home"
+        CmdCustomStatusOffice = "office"
+        CmdCustomStatusMeeting = "meeting"
 
 	DefaultCustomStatusEmoji = "speech_balloon"
 )
@@ -37,8 +42,42 @@ func (*CustomStatusProvider) GetCommand(a *app.App, T goi18n.TranslateFunc) *mod
 		AutoComplete:     true,
 		AutoCompleteDesc: T("api.command_custom_status.desc"),
 		AutoCompleteHint: T("api.command_custom_status.hint"),
+                AutocompleteData: GetCommandAutoCompleteData(T),
 		DisplayName:      T("api.command_custom_status.name"),
 	}
+}
+
+func GetCommandAutoCompleteData(T goi18n.TranslateFunc) *model.AutocompleteData {
+        command := model.NewAutocompleteData(CmdCustomStatus, "", T("api.command_custom_status.desc"))
+        command.AddStaticListArgument("", true, []model.AutocompleteListItem{
+                {
+                        Item: CmdCustomStatusClear,
+                        HelpText: "Clear your custom status",
+                }, {
+                        Item: CmdCustomStatusPrev,
+                        HelpText: "Set previous custom status",
+                }, {
+                        Item: CmdCustomStatusAway,
+                        Hint: "[HH:MM] [message]",
+                        HelpText: "Set your custom status to 'Away'",
+                }, {
+                        Item: CmdCustomStatusHome,
+                        Hint: "[message]",
+                        HelpText: "Set your custom status to 'Working from home'",
+                }, {
+                        Item: CmdCustomStatusOffice,
+                        Hint: "[message]",
+                        HelpText: "Set your custom status to 'At the office'",
+                }, {
+                        Item: CmdCustomStatusMeeting,
+                        Hint: "[HH:MM] [message]",
+                        HelpText: "Set your custom status to 'In a meeting'",
+                }, {
+                        Hint: "[:emoji_name:] [message]",
+                        HelpText: "Set custom status",
+                },
+        })
+        return command
 }
 
 func (*CustomStatusProvider) DoCommand(a *app.App, args *model.CommandArgs, message string) *model.CommandResponse {
@@ -46,7 +85,7 @@ func (*CustomStatusProvider) DoCommand(a *app.App, args *model.CommandArgs, mess
 		return nil
 	}
 
-	if message == CmdCustomStatusClear {
+	if strings.HasPrefix(message, CmdCustomStatusClear) {
 		if err := a.RemoveCustomStatus(args.UserId); err != nil {
 			mlog.Error(err.Error())
 			return &model.CommandResponse{Text: args.T("api.command_custom_status.clear.app_error"), ResponseType: model.COMMAND_RESPONSE_TYPE_EPHEMERAL}
@@ -57,17 +96,76 @@ func (*CustomStatusProvider) DoCommand(a *app.App, args *model.CommandArgs, mess
 			Text:         args.T("api.command_custom_status.clear.success"),
 		}
 	}
+        if strings.HasPrefix(message, CmdCustomStatusPrev) {
+                if err := a.SetPrevRecentCustomStatus(args.UserId); err != nil {
+			mlog.Error(err.Error())
+			return &model.CommandResponse{Text: args.T("api.command_custom_status.app_error"), ResponseType: model.COMMAND_RESPONSE_TYPE_EPHEMERAL}
+                }
+                cs, err := a.GetCustomStatus(args.UserId)
+                if err != nil {
+			mlog.Error(err.Error())
+			return &model.CommandResponse{Text: args.T("api.command_custom_status.app_error"), ResponseType: model.COMMAND_RESPONSE_TYPE_EPHEMERAL}
+                }
+                if cs == nil {
+                        return &model.CommandResponse{
+                                ResponseType: model.COMMAND_RESPONSE_TYPE_EPHEMERAL,
+                                Text:         args.T("api.command_custom_status.clear.success"),
+                        }
+                }
+                return &model.CommandResponse{
+                        ResponseType: model.COMMAND_RESPONSE_TYPE_EPHEMERAL,
+                        Text: args.T("api.command_custom_status.success", map[string]interface{}{
+                                "EmojiName":     ":" + cs.Emoji + ":",
+                                "StatusMessage": cs.Text,
+                        }),
+                }
+        }
 
 	customStatus := &model.CustomStatus{
 		Emoji: DefaultCustomStatusEmoji,
 		Text:  message,
 	}
-	firstEmojiLocations := model.ALL_EMOJI_PATTERN.FindIndex([]byte(message))
-	if len(firstEmojiLocations) > 0 && firstEmojiLocations[0] == 0 {
-		// emoji found at starting index
-		customStatus.Emoji = message[firstEmojiLocations[0]+1 : firstEmojiLocations[1]-1]
-		customStatus.Text = strings.TrimSpace(message[firstEmojiLocations[1]:])
-	}
+
+        if strings.HasPrefix(message, CmdCustomStatusAway) {
+                customStatus.Emoji = "car"
+                customStatus.Text  = "Away"
+                msg := strings.TrimPrefix(message, CmdCustomStatusAway)
+                msg  = strings.TrimSpace(msg)
+                if msg != "" {
+                        customStatus.Text += " (" + msg + ")"
+                }
+        } else if strings.HasPrefix(message, CmdCustomStatusHome) {
+                customStatus.Emoji = "house"
+                customStatus.Text  = "Working from home"
+                msg := strings.TrimPrefix(message, CmdCustomStatusHome)
+                msg  = strings.TrimSpace(msg)
+                if msg != "" {
+                        customStatus.Text += " (" + msg + ")"
+                }
+        } else if strings.HasPrefix(message, CmdCustomStatusOffice) {
+                customStatus.Emoji = "office"
+                customStatus.Text  = "At the office"
+                msg := strings.TrimPrefix(message, CmdCustomStatusOffice)
+                msg  = strings.TrimSpace(msg)
+                if msg != "" {
+                        customStatus.Text += " (" + msg + ")"
+                }
+        } else if strings.HasPrefix(message, CmdCustomStatusMeeting) {
+                customStatus.Emoji = "telephone"
+                customStatus.Text  = "In a meeting"
+                msg := strings.TrimPrefix(message, CmdCustomStatusMeeting)
+                msg  = strings.TrimSpace(msg)
+                if msg != "" {
+                        customStatus.Text += " (" + msg + ")"
+                }
+        } else {
+                firstEmojiLocations := model.ALL_EMOJI_PATTERN.FindIndex([]byte(message))
+                if len(firstEmojiLocations) > 0 && firstEmojiLocations[0] == 0 {
+                        // emoji found at starting index
+                        customStatus.Emoji = message[firstEmojiLocations[0]+1 : firstEmojiLocations[1]-1]
+                        customStatus.Text = strings.TrimSpace(message[firstEmojiLocations[1]:])
+                }
+        }
 
 	customStatus.TrimMessage()
 	if err := a.SetCustomStatus(args.UserId, customStatus); err != nil {
